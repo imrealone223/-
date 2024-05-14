7f8290110f0e46fcec5dbf18c8a428c802de5f73
SakhAlgo <sakhalgo@gmail.com>
2024-05-14 21:10:06 +1100

    add functional

--- functional.js
+++ functional.js
@@ -7,52 +7,61 @@ let delphin1 = document.querySelector('.delphin')
 let meduza={
     name:'Медуза',
     price: 3999,
-    photo:'https://en.wikipedia.org/wiki/Jellyfish'
+    photo:'https://images.unsplash.com/photo-1628944682084-831f35256163?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
 }
 let horse={
     name:'Морская лошадка',
     price: 12999,
-    photo:"https://en.wikipedia.org/wiki/Seahorse",
+    photo:"https://images.unsplash.com/photo-1515309025403-4b0184873cef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
 }
 let osminog={
     name:'Осьминог',
     price: 15999,
-    photo:"https://en.wikipedia.org/wiki/Octopus",
+    photo:"https://images.unsplash.com/photo-1545671913-b89ac1b4ac10?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
 }
 let shark={
     name:'Акула',
     price: 57999,
-    photo:"https://en.wikipedia.org/wiki/Shark",
+    photo:"https://images.unsplash.com/photo-1598977123118-4e30ba3c4f5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
 }
 let delphin={
     name:'Дельфин',
     price: 18999,
-    photo:"https://en.wikipedia.org/wiki/Dolphin",
+    photo:"https://images.unsplash.com/photo-1570481662006-a3a1374699e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
 }
 
 let products = [meduza, horse, osminog, shark, delphin]
 let basket_array = []
 
 meduza1.addEventListener('click',function(){
-    korz.push(meduza)
+    basket_array.push(meduza)
 })
 horse1.addEventListener('click',function(){
-    korz.push(horse)
+    basket_array.push(horse)
 })
 osminog1.addEventListener('click',function(){
-    korz.push(osminog)
+    basket_array.push(osminog)
 })
 shark1.addEventListener('click',function(){
-    korz.push(shark)
+    basket_array.push(shark)
 })
 delphin1.addEventListener('click',function(){
-    korz.push(delphin)
+    basket_array.push(delphin)
 })
 
 let basket = document.querySelector('.basket')
 let main = document.querySelector('.main')
 let basket_list = document.querySelector('.basket-list')
+let result = 0
 
 basket.addEventListener('click', function(){
     main.style.display = 'none'
+    basket_list.innerHTML = '<hr>'
+    for(let i=0; i<basket_array.length; i++){
+        basket_list.innerHTML = basket_list.innerHTML + `<h3 width="300px">${i+1}.  ${basket_array[i].name}</h3> <img height="50px" src="${basket_array[i].photo}"/img><h4>  ${basket_array[i].price} руб.</h4><hr>`
+        result = +result + +basket_array[i].price
+        
+    }
+    basket_list.innerHTML = basket_list.innerHTML + `<h3 width="300px">Итого:  ${result} руб.<h3>`
+    
 })
